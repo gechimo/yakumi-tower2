801434169210ca7860dc7c815a35e24f244e872f
gechimo <gechimomonga@gmail.com>
2026-01-13 01:55:22 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     // GitHub Pagesで公開する場合、リポジトリ名に合わせてbaseを設定します
     // 例: https://<username>.github.io/<repository>/ なら base: '/<repository>/'
-    base: './',
+    base: '/yakumi-tower2/',
     build: {
         outDir: 'dist',
     }
